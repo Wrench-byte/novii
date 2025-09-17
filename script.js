2d590de1f8d3c41e8c73f6112cca1f08f7270d3c
Wrench <jasonbrody23far@gmail.com>
2025-09-17 04:42:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+function acces(){
+   let date = document.getElementById('data').value
+    if(date == "12032025"){
+       window.open("pag1.html")
+        window.close()
+    } else{
+        alert("Неправильно")
+    }
+   
+   
+}
+
+const imagen = document.getElementById('miImagen');
+const imagen2 = document.getElementById('miImagen2')      
+const imagen3 = document.getElementById('miImagen3')
+const imagen4 = document.getElementById('miImagen4')
+const imagen5 = document.getElementById('miImagen5')
+const imagen6 = document.getElementById('miImagen6') 
+        imagen.addEventListener('click', function() {
+            this.classList.toggle('ampliada');
+        });
+        imagen2.addEventListener('click', function() {
+            this.classList.toggle('ampliada');
+        });
+        imagen3.addEventListener('click', function() {
+            this.classList.toggle('ampliada');
+        });
+        imagen4.addEventListener('click', function() {
+            this.classList.toggle('ampliada');
+        });imagen5.addEventListener('click', function() {
+            this.classList.toggle('ampliada');
+        });imagen6.addEventListener('click', function() {
+            this.classList.toggle('ampliada');
+        });
+
+   function bap() {
+    setTimeout(tup, 1000)
+    
+}
+ function tup(){
+    alert("Милое, мне особо нравится, станешь девочкам сразу")
+ }
+
+ function bip() {
+    setTimeout(tap, 1000)
+    
+}
+ function tap(){
+    alert("Красотка, просто очень сильно красивая")
+ }
+ 
+function bup() {
+    setTimeout(blu, 1000)
+    
+}
+ function blu(){
+    alert("Принцесса, прям из сказки вышла))")
+ }
+
+ function glu() {
+    setTimeout(wao, 1000)
+    
+}
+ function wao(){
+    alert("Офигеть)))))")
+ }
+
+ function aaa() {
+    setTimeout(tip, 1000)
+    
+}
+ function tip(){
+    alert(".....))))))🔥🔥🔥🔥🔥🔥")
+ }
+
+ function bce() {
+    setTimeout(taa, 1000)
+    
+}
+ function taa(){
+    alert("Любя, доказательство русская любовь;)")
+ }
+   
+
+    
+      
+      
+    
+
+    
